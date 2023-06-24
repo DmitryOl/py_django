d722b1bee436340d57973a93484ee044d722b919
wride <wride@yandex.ru>
2023-06-24 21:50:04 +0000

    new project start

---
+++ blog/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+posts = [
+	{
+    	'author': 'Администратор',
+    	'title': 'Это первый пост',
+    	'content': 'Содержание первого поста.',
+    	'date_posted': '12 мая, 2022'
+	},
+	{
+    	'author': 'Пользователь',
+    	'title': 'Это второй пост',
+    	'content': 'Подробное содержание второго поста.',
+    	'date_posted': '13 мая, 2022'
+	}
+]
+
+def home(request):
+	context = {
+    	'posts': posts
+	}
+	return render(request, '/home/dmitry/djangoPro/blog/templates/home.html', context)
+#    return HttpResponse('<h1>Главная</h1>')
+
+def about(request):
+	return render(request, '/home/dmitry/djangoPro/blog/templates/about.html', {'title': 'О клубе Python Bytes'})
+#    return HttpResponse('<h1>Наш клуб</h1>')
+
+
+# Create your views here.
+
